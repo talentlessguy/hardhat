00bd271dc36f0976a3278cd15ad1fab5e8979771
Igor Matuszewski <xanewok@gmail.com>
2024-08-21 21:46:10 +0200

    WIP: Work around HH 2.22.6

--- packages/hardhat-core/src/internal/hardhat-network/stack-traces/message-trace.ts
+++ packages/hardhat-core/src/internal/hardhat-network/stack-traces/message-trace.ts
@@ -10,3 +10,24 @@ export type MessageTrace =
   | CreateMessageTrace
   | CallMessageTrace
   | PrecompileMessageTrace;
+
+// WIP: Remove all of these once the feature branch is bumped above HH v2.22.6
+export type EvmMessageTrace = CreateMessageTrace | CallMessageTrace;
+export function isPrecompileTrace(
+  trace: MessageTrace
+): trace is PrecompileMessageTrace {
+  return "precompile" in trace;
+}
+
+export function isCallTrace(trace: MessageTrace): trace is CallMessageTrace {
+  return "code" in trace && "calldata" in trace;
+}
+
+export interface EvmStep {
+  pc: number;
+}
+
+export type MessageTraceStep = MessageTrace | EvmStep;
+export function isEvmStep(step: MessageTraceStep): step is EvmStep {
+  return "pc" in step && step.pc !== undefined;
+}
